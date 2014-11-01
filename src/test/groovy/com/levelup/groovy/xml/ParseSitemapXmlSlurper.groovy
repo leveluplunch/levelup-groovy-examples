a4c28a670950087ac4239aecb60856874acf1aaa
Justin Musgrove <justin.musgrove@gmail.com>
2014-11-01 10:10:40 -0500

    added parse site map with xmlslurper

---
+++ src/test/groovy/com/levelup/groovy/xml/ParseSitemapXmlSlurper.groovy
@@ -0,0 +1,27 @@
+package com.levelup.groovy.xml
+
+import org.junit.Test
+
+/**
+ * This groovy example will demonstrate how to read rss with XMLSlurper
+ *
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com/groovy/examples/parse-sitemap-xml-with-xmlsluper/'>Parse sitemap with XmlSlurper</a>
+ */
+class ParseSitemapXmlSlurper {
+
+    @Test
+    public void parse_rss_xml_feed() {
+
+        def siteMapLocation = "http://www.leveluplunch.com/sitemap.xml".toURL().text
+
+        def urlset = new XmlSlurper().parseText(siteMapLocation)
+        urlset.url.each{
+            println it.loc
+            println it.lastmod
+            println it.priority
+            println "^^^^^^^^"
+        }
+    }
+
+}
